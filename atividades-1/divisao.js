8b8fe6287d02631565c5cd72dc0c50ee962ec83d
Chart 70 <williamitalia70@outlook.com>
2024-04-27 00:57:05 -0300

    up

--- atividades-1/divisao.js
+++
@@ -1,8 +0,0 @@
-//baixar extensão Code Runner e utilizar o comando: CTRL + ALT + N
- 
-function divisao(numerador, denominador) {
-    return numerador / denominador;
-}
-
-console.log(divisao(12, 3));
- 
